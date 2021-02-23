872d84ce17de7b7592a024a21496439312a98a9b
Carmelo Ruymán Quintana Santana <>
2021-02-23 20:49:06 +0100

    First commit

---
+++ Shared/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  Shared
+//
+//  Created by Carmelo Ruymán Quintana Santana on 23/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
